0fe700210ef4d462a210edeab1a522976b6e0b28
石头老师 <shitou1235813@163.com>
2019-11-04 08:50:22 +0800

    Add files via upload

---
+++ 39结构体定义和使用.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+
+//结构体定义
+struct student
+{
+	//成员列表
+	string name;
+	int age;
+	int score;
+
+}stu3;
+
+int main39() {
+
+	//结构体变量创建方式1
+	struct student stu1;//struct 关键字可以省略
+	stu1.name = "张三";
+	stu1.age = 18;
+	stu1.score = 100;
+
+	cout << "姓名" << stu1.name << "年龄" << stu1.age << "分数" << stu1.score << endl;
+
+	//结构体变量创建方式2
+	student stu2 = { "李四",19,60 };
+	stu2.name = "李四";
+	stu2.age = 19;
+	stu2.score = 60;
+
+	cout << "姓名" << stu2.name << "年龄" << stu2.age << "分数" << stu2.score << endl;
+
+	//结构体变量创建方式3
+	
+	stu3.name = "王五";
+	stu3.age = 18;
+	stu3.score = 80;
+
+	cout << "姓名" << stu3.name << "年龄" << stu3.age << "分数" << stu3.score << endl;
+	system("pause");
+	return 0;
+}
